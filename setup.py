fabb225449c4f0b9f8bfde0a7e33f2ab3ebaaf51
Chiwon Cho <goldworm@icon.foundation>
2018-11-02 15:55:40 +0900

    VERSION: 0.0.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="icondbtools",
-    version="0.0.1",
+    version="0.0.2",
     author="ICON Foundation",
     author_email="goldworm@icon.foundation",
     description="icon db tools",
