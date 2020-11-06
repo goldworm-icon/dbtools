5d6614289d196bebe94287f587e8954bd4c38569
inwon <37284520+inwonkim@users.noreply.github.com>
2020-11-06 12:41:11 +0900

    modify totalbalance command (#26)
    * consider fee2.0 deposit
    
    Co-authored-by: inwonkim <inwonkim@icon.foundation>

--- icondbtools/command/command_total_balance.py
+++ icondbtools/command/command_total_balance.py
@@ -13,14 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import TYPE_CHECKING
+from iconservice.base.address import Address
+from iconservice.fee.deposit import Deposit
+from iconservice.utils.msgpack_for_db import MsgPackForDB
 
 from icondbtools.command.command import Command
 from icondbtools.libs.state_database_reader import StateDatabaseReader
-from iconservice.base.address import Address
 
-if TYPE_CHECKING:
-    from iconservice.icx.icx_account import Account
+MSG_PACK_DEPOSIT_DATA_LENGTH = 11
 
 
 class CommandTotalBalance(Command):
@@ -47,6 +47,8 @@ class CommandTotalBalance(Command):
         total_staked = 0
         active_account_count = 0
         staking_account_count = 0
+        total_deposit = 0
+        deposit_count = 0
 
         address = None
 
@@ -57,37 +59,42 @@ class CommandTotalBalance(Command):
             i = 0
             errors = 0
             for key, value in iterator:
-                if not is_account_key(key):
-                    continue
-
                 try:
-                    address = Address.from_bytes(key)
-                    #print(f"{i}: {address}")
+                    if is_account_key(key):
+                        address = Address.from_bytes(key)
+                        coin_part = reader.get_coin_part(address)
+                        stake_part = reader.get_stake_part(address)
+                        if coin_part is not None:
+                            active_account_count += 1
+                            total_balance += coin_part.balance
+                        if stake_part is not None:
+                            staking_account_count += 1
+                            total_staked += stake_part.total_stake
+                    elif is_fee2_key(key):
+                        if is_deposit(value):
+                            deposit = Deposit.from_bytes(value)
+                            total_deposit += deposit.deposit_amount
+                            deposit_count += 1
+                    else:
+                        continue
 
-                    coin_part = reader.get_coin_part(address)
-                    stake_part = reader.get_stake_part(address)
-
-                    if coin_part is not None:
-                        active_account_count += 1
-                        total_balance += coin_part.balance
-                    if stake_part is not None:
-                        staking_account_count += 1
-                        total_staked += stake_part.total_stake
-                except BaseException as e:
+                except BaseException:
                     errors += 1
 
                 i += 1
 
-            full_balance = total_balance + total_staked
+            full_balance = total_balance + total_staked + total_deposit
             total_supply = reader.get_total_supply()
             print(
                 f"total supply : {total_supply:30,}\n"
                 f"total balance: {full_balance:30,}\n"
                 f"      balance: {total_balance:30,}\n"
                 f"      staked : {total_staked:30,}\n"
+                f"      deposit: {total_deposit:30,}\n"
                 f"diff         : {full_balance - total_supply:,}\n"
                 f"active account count : {active_account_count:,}\n"
                 f"staking account count : {staking_account_count:,}\n"
+                f"deposit         count : {deposit_count:,}\n"
                 f"errors: {errors}"
             )
         except Exception as e:
@@ -104,4 +111,19 @@ def is_account_key(key: bytes) -> bool:
     elif key_length == 21:
         if key[0] == 1:
             return True
+    return False
+
+
+def is_fee2_key(key: bytes) -> bool:
+    key_length = len(key)
+    if key_length == 33:
+        if key[0] == 2:
+            return True
+    return False
+
+
+def is_deposit(value: bytes) -> bool:
+    data = MsgPackForDB.loads(value)
+    if len(data) == MSG_PACK_DEPOSIT_DATA_LENGTH:
+        return True
     return False
